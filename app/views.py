50a137347aa382a2d494d7037b9bc87fcda59fd4
TheKhera <thekheragame@gmail.com>
2015-06-20 18:13:54 +0200

    Post render
    Ora i post vengono visulizzati nella homepage tramite un ciclo for ed un
    array dei post.

--- app/views.py
+++ app/views.py
@@ -4,4 +4,20 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Test Page')
+	listPost = [ #Falsi array dei post
+		{
+			'autore': 'AdminTest',
+			'title': 'Test title',
+			'body': 'Semplice prova'
+		},
+		{
+			'autore': 'AdminTest',
+			'title': 'Test title 2',
+			'body': 'Test Alpha'
+		}
+	]
+	return render_template('index.html', title='Test Page', posts=listPost)
+
+@app.route('/About')
+def about():
+	return render_template('about.html', title='About')
