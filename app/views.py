cd6f73783671050fe8c00d2101b9bda012eff5ed
TheKhera <thekheragame@gmail.com>
2015-06-30 18:31:52 +0200

    Aggiunto un editor. Attenzione! Abbiamo eliminato tutti gli aggionamenti precedeti.

--- app/views.py
+++ app/views.py
@@ -1,23 +1,52 @@
-from flask import render_template
+from flask import render_template, Flask, jsonify, request
 from app import app
+import json
 
 @app.route('/')
-@app.route('/index')
 def index():
-	listPost = [ #Falsi array dei post
-		{
-			'autore': 'AdminTest',
-			'title': 'Test title',
-			'body': 'Semplice prova'
-		},
-		{
-			'autore': 'AdminTest',
-			'title': 'Test title 2',
-			'body': 'Test Alpha'
-		}
-	]
-	return render_template('index.html', title='Test Page', posts=listPost)
+	return render_template('index.html')
 
 @app.route('/About')
 def about():
 	return render_template('about.html', title='About')
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+@app.route('/_test_save')
+def test_save():
+    autore = request.args.get('autore', 0, type=str)
+    data = request.args.get('data', 0, type=str)
+    corpo = request.args.get('corpo', 0, type=str)
+
+    print corpo
+    
+    corpo = encode(corpo, "utf-8") # Convert for debug test
+    
+    data = [
+        {'autore': autore,
+         'data': data,
+         'corpo': corpo
+         }
+    ]
+
+    
+    filePostOpen = open('database/POST/#_nPost.txt', 'r')
+
+    nPostStr = filePostOpen.read()
+    nPost = int(nPostStr)
+    nPost += 1
+    filePostOpen.close()
+
+    with open('database/POST/pst_ ' + str(nPost) + '.json', 'w') as outputFile:
+        json.dump(data, outputFile)
+    outputFile.close()
+
+    filePostOpen2 = open('database/POST/#_nPost.txt', 'w')
+    filePostOpen2.write(str(nPost))
+    filePostOpen2.close()
+
+    return jsonify(result="Post pubblicato!")
