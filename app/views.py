af5f6dff2ddc28ef75146f3e494c09ac334a9f38
TheKhera <thekheragame@gmail.com>
2015-07-01 19:24:44 +0200

    Aggiunta: /homepage, /posteditor

--- app/views.py
+++ app/views.py
@@ -3,32 +3,29 @@ from app import app
 import json
 
 @app.route('/')
+@app.route('/homepage')
 def index():
-	return render_template('index.html')
+	return render_template('homepage.html')
+
+@app.route('/posteditor')
+def posteditor():
+	return render_template('post_editor.html')
 
 @app.route('/About')
 def about():
 	return render_template('about.html', title='About')
 
-@app.route('/_add_numbers')
-def add_numbers():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
-
 @app.route('/_test_save')
 def test_save():
     autore = request.args.get('autore', 0, type=str)
     data = request.args.get('data', 0, type=str)
+    titolo = request.args.get('titolo', 'Titolo Post', type=str)
     corpo = request.args.get('corpo', 0, type=str)
-
-    print corpo
-    
-    corpo = encode(corpo, "utf-8") # Convert for debug test
     
     data = [
         {'autore': autore,
          'data': data,
+         'titolo': titolo,
          'corpo': corpo
          }
     ]
