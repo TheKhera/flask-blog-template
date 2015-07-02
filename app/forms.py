30b22831ad496d6b7426ed55e8fc9b1802432345
TheKhera <thekheragame@gmail.com>
2015-07-02 15:38:34 +0200

    Aggiunto e corretto form per la registrazione

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
 
 class MyRegForm(Form):
-    username = TextField('Username', [validators.Length(min=6, max=32)])
+    username = TextField('Username',[validators.Length(min=6, max=32,message='Username deve essere lungo da 6 a 32 caratteri.')])
     email = TextField('Indirizzo E-mail', [validators.Length(min=6, max=35)])
     password = PasswordField('Nuova Password', [
         validators.Required(),
