c790bfac81f0fdf4b66abc448a4eeb23b0ffd950
TheKhera <thekheragame@gmail.com>
2015-07-06 14:26:38 +0200

    Aggiunte voci

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-#File di configurazione
-
-DEBUG = True
+# Test Config
 WTF_CSRF_ENABLE = True
-SECRET_KEY = 'un-mio-segreto-magico'
+SECRET_KEY = 'key-di-sviluppo'
+
+# Cambiare i seguenti parametri
+USER='admin' # Nome per accedere alla parte amministrativa
+PASS='admin' # Password per accedere alla parte amministrativa
+
+SITE_TITLE = 'Flask-Blog-Template' # Nome del sito
+SITE_SUBTITLE = '' # Sotto Titolo del sito (NB. Attualemnte non implementato)
